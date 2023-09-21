2e042f295549d54d48d20662e4c5a45724db6961
Windula Dissanayake <charithwinwindula@gmail.com>
2023-09-21 13:43:00 +0530

    commit

--- src/main/java/com/example/webapplication/service/StudentService.java
+++ src/main/java/com/example/webapplication/service/StudentService.java
@@ -5,11 +5,16 @@ import com.example.webapplication.entity.Student;
 import java.util.List;
 
 public interface StudentService {
+
     List<Student> getAllStudents();
+
     Student addStudent(Student student);
 
     Student getStudentById(int id);
+
     Student updateStudent(Student student);
 
     void deleteStudentById(int id);
+
+    List<Student> findStudentByKeyword(String StudentId);
 }
